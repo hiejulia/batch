4b5a158f02bc399485652b878d3384ccb3b8752e
hiennguyen@bookygroup.fi <hienminhnguyen711@gmail.com>
2019-01-24 16:49:16 +0200

    add apache kafka docker script

--- scripts/apache_kafka/apache_kafka.sh
+++ scripts/apache_kafka/apache_kafka.sh
@@ -3,7 +3,7 @@
 
 # Pull Apache kafka image
 
-
+docker run -p 2181:2181 -p 9092:9092 --env ADVERTISED_HOST=`docker-machine ip \`docker-machine active\`` --env ADVERTISED_PORT=9092 spotify/kafka
 
 
 # Start apache kafka container
