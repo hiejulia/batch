17fe8f46e4b0ebde8f9652255113d9b9013e8ec1
hiennguyen@bookygroup.fi <hienminhnguyen711@gmail.com>
2019-01-28 09:16:34 +0200

    Update to writer

---
+++ src/main/java/com/project/batch/writer/transaction/StatementSuffixGenerator.java
@@ -0,0 +1,13 @@
+package com.project.batch.writer.transaction;
+
+
+import org.springframework.batch.item.file.ResourceSuffixCreator;
+
+public class StatementSuffixGenerator implements ResourceSuffixCreator {
+
+    public String getSuffix(int arg0) {
+        return arg0 + ".txt";
+    }
+
+}
+// No khong muon yeu duongdau
