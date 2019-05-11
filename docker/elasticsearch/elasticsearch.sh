606ed5f03349e1d396febec73e8fd9d136ae4add
Hien <hienminhnguyen711@gmail.com>
2019-05-11 12:48:03 +0300

    update

--- docker/elasticsearch/elasticsearch.sh
+++ docker/elasticsearch/elasticsearch.sh
@@ -4,5 +4,3 @@
 # Run ElasticSearch docker image 
 docker run -p 9200:9200 -e "http.host=0.0.0.0" -e "transport.host=127.0.0.1" docker.elastic.co/elasticsearch/elasticsearch:5.4.3
 
-
-# vai chuong con nho nay - nghe dream theater 
